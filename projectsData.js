09beb5c0d3b65f52a6a9a1d8def513e6028a0c1f
William Krolik <wkrolik@InComm.com>
2019-07-15 10:55:08 -0700

    m "add data file"

---
+++ projectsData.js
@@ -0,0 +1,10 @@
+var myProjects = {
+    projects: [
+        {"id":0,
+        "project_name":"Random Quote Generator",
+        "description":"A random quote generator that pulls different quotes from Spiderman:Into the SPiderverse when the button is clicked",
+        "technologies": ["HTML","Vanilla JS","CSS"],
+        "live_link": "https://willkrolik.github.io/Tech-Degree-Project-1/"
+    }
+    ]
+};
